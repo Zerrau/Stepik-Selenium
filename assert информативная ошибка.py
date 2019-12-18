4a9941b11d428af78688f1e5604aa041382b324e
Zerrau <mogilinsa@mail.ru>
2019-12-18 08:18:00 +0300

    assert информативная ошибка.py - leson 3.2.8

--- assert информативная ошибка.py
+++ assert информативная ошибка.py
@@ -1,4 +1,13 @@
-def test_input_text(expected_result, actual_result):
+"""def test_input_text(expected_result, actual_result):
     assert expected_result == actual_result, \
-        f"expected  {expected_result} got  {actual_result}"
+        f"expected  {expected_result} got  {actual_result}" """
 
+
+def test_substring(full_string, substring):
+    assert substring not in full_string
+    print("expected '{}' to be substring of '{}'".format(substring, full_string))
+
+# c = test_substring(a, b)
+# print(c)
+# print('expected \'substring\' to be substring of \'full_string\'')
+# print('Here is \'some text in quotes\'. Here is text without quotes.')
