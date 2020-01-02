3d41331f2859c7d39b83e28017af2bbdd5472b4a
Zerrau <mogilinsa@mail.ru>
2020-01-02 16:05:51 +0300

    unittest rename readme.md

--- run_test_unittest.py
+++ run_test_unittest.py
@@ -1,12 +1,36 @@
+import time
 import unittest
+from selenium import webdriver
 
 
 class TestAbs(unittest.TestCase):
     def test_abs1(self):
-        self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+        link1 = 'http://suninjuly.github.io/registration1.html'
+        browser = webdriver.Chrome()
+        browser.get(link1)
+        browser.find_element_by_xpath('/html/body/div/form/div[1]/div[1]/input').send_keys('Иван')
+        browser.find_element_by_xpath('/html/body/div/form/div[1]/div[2]/input').send_keys('Иванов')
+        browser.find_element_by_xpath('/html/body/div/form/div[1]/div[3]/input').send_keys('ivan@iv.com')
+        browser.find_element_by_xpath('/html/body/div/form/button').click()
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element_by_xpath('/html/body/div/h1')
+        welcome_text = welcome_text_elt.text
+        self.assertEqual(welcome_text, 'Congratulations! You have successfully registered!')
 
     def test_abs2(self):
-        self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
+        link2 = 'http://suninjuly.github.io/registration2.html'
+        browser = webdriver.Chrome()
+        browser.get(link2)
+        browser.find_element_by_xpath('/html/body/div/form/div[1]/div[1]/input').send_keys('Иван')
+        browser.find_element_by_xpath('/html/body/div/form/div[1]/div[2]/input').send_keys('Иванов')
+        browser.find_element_by_xpath('/html/body/div/form/div[1]/div[3]/input').send_keys('ivan@iv.com')
+        browser.find_element_by_xpath('/html/body/div/form/button').click()
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element_by_xpath('/html/body/div/h1')
+        welcome_text = welcome_text_elt.text
+        self.assertEqual(welcome_text, welcome_text_elt.text, 'Congratulations! You have successfully registered!')
 
 
 if __name__ == "__main__":
