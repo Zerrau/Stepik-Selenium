4252ce4db41a0abdf62fffa94cbf4fd0edbdb2bf
Zerrau <mogilinsa@mail.ru>
2020-01-29 22:52:58 +0300

    add leason 3.6.3

--- test_leson_363.py
+++ test_leson_363.py
@@ -5,17 +5,17 @@ import unittest
 import pytest
 
 
-@pytest.mark.parametrize('link', ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1",
-                                  "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1",
-                                  "https://stepik.org/lesson/236899/step/1", "https://stepik.org/lesson/236903/step/1",
-                                  "https://stepik.org/lesson/236904/step/1", "https://stepik.org/lesson/236905/step/1"])
 class SubclassesTest(unittest.TestCase):
+    browser = webdriver.Chrome()
 
+    @pytest.mark.parametrize('link',
+                             ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1",
+                              "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1",
+                              "https://stepik.org/lesson/236899/step/1", "https://stepik.org/lesson/236903/step/1",
+                              "https://stepik.org/lesson/236904/step/1", "https://stepik.org/lesson/236905/step/1"])
     def test_One(browser, link):
         browser.get(link)
-        browser = webdriver.Chrome()
         answer = str(math.log(int(time.time())))
-        link = 'https://stepik.org/lesson/236903/step/1'
 
         browser.implicitly_wait(5)
         try:
