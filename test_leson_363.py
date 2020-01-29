6bff6a5dd726f014ba4dd7d1ca1f6db2f06cda8d
Zerrau <mogilinsa@mail.ru>
2020-01-29 22:57:23 +0300

    add leason 3.6.3

--- test_leson_363.py
+++ test_leson_363.py
@@ -5,15 +5,15 @@ import unittest
 import pytest
 
 
+@pytest.mark.parametrize('link',
+                         ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1",
+                          "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1",
+                          "https://stepik.org/lesson/236899/step/1", "https://stepik.org/lesson/236903/step/1",
+                          "https://stepik.org/lesson/236904/step/1", "https://stepik.org/lesson/236905/step/1"])
 class SubclassesTest(unittest.TestCase):
-    browser = webdriver.Chrome()
 
-    @pytest.mark.parametrize('link',
-                             ["https://stepik.org/lesson/236895/step/1", "https://stepik.org/lesson/236896/step/1",
-                              "https://stepik.org/lesson/236897/step/1", "https://stepik.org/lesson/236898/step/1",
-                              "https://stepik.org/lesson/236899/step/1", "https://stepik.org/lesson/236903/step/1",
-                              "https://stepik.org/lesson/236904/step/1", "https://stepik.org/lesson/236905/step/1"])
-    def test_One(browser, link):
+    def test_One(link):
+        browser = webdriver.Chrome()
         browser.get(link)
         answer = str(math.log(int(time.time())))
 
@@ -35,3 +35,17 @@ class SubclassesTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
